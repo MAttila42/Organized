ab644e4db0c9de493ea62b8d5f573c98f6c8296d
MAttila42 <65223119+MAttila42@users.noreply.github.com>
2025-11-06 14:12:03 +0100

    ➕ add: shadcn components

---
+++ packages/web/src/lib/components/ui/accordion/accordion-content.svelte
@@ -0,0 +1,23 @@
+<script lang='ts'>
+  import type { WithoutChild } from '$lib/utils'
+  import { cn } from '$lib/utils'
+  import { Accordion as AccordionPrimitive } from 'bits-ui'
+
+  let {
+    ref = $bindable(null),
+    class: className,
+    children,
+    ...restProps
+  }: WithoutChild<AccordionPrimitive.ContentProps> = $props()
+</script>
+
+<AccordionPrimitive.Content
+  bind:ref
+  data-slot='accordion-content'
+  class='overflow-hidden text-sm data-[state=open]:animate-accordion-down data-[state=closed]:animate-accordion-up'
+  {...restProps}
+>
+  <div class={cn('pb-4 pt-0', className)}>
+    {@render children?.()}
+  </div>
+</AccordionPrimitive.Content>
